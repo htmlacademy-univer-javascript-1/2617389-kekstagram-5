4ad2b1142926b0bff492526c9ec869bb80d83a55
Sergey Nesterenko <47058928+zeov1@users.noreply.github.com>
2025-06-16 23:14:22 +0300

    функции для строк

---
+++ js/functions.js
@@ -0,0 +1,4 @@
+const isStringValid = (string, length) => string.length <= length;
+
+const isStringPalindrome = (string) => string.toUpperCase().split('').filter() === string.toUpperCase().split('').reverse().join('');
+
